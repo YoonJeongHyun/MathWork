e18d9d534c48d156c4fad995cdf32bab7e57219a
sublime <jeonghyun.yoon@hd.com>
2025-06-15 18:31:34 +0900

    remove say hello function in calculator.py

--- calculator.py
+++ calculator.py
@@ -4,17 +4,8 @@ def add(a, b):
   return a+b
 def subtract(a, b):
   return a-b
-  
-<<<<<<< HEAD
+
 def divide_premium(a,b):
-=======
-def divide_free(a,b):
->>>>>>> main
+
   return a/b
 
-def sex(a,b):
-  return a,b
-
-
-def say_hello():
-  print("say hello")
